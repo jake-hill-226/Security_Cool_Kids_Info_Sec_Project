58f7c0eaaaf17909c0ead8e682ede5df5d54da3e
greek-stasia <bourlas.3@osu.edu>
2017-10-25 22:41:13 -0400

    Update vault_encrypt.py

--- vault_encrypt.py
+++ vault_encrypt.py
@@ -1,6 +1,11 @@
 #!usr/bin/python
 
 from Crypto.Cipher import AES
+import random
+
+chars = 'abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()'
+len = 4
+
 def encrypt(pwd){
     obj = AES.new(pwd, AES.MODE_CBC, 'This is an IV456')
     message = pwd
@@ -11,4 +16,14 @@ def encrypt(pwd){
 def decrypt(cipheredpwd, pwd){
     obj2 = AES.new(pwd, AES.MODE_CBC, 'This is an IV456')
     return(obj2.decrypt(cipheredpwd))
+}
+#Generate arbitrary password
+def pwd_gen(){
+    pwd = ''
+    for x in range(0,len):
+        for x in range(0,len):
+            pwd += random.choice(chars)
+        pwd += '-'
+    encrypt(pwd)
+
 }
