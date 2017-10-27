a4e09c92ed85c5924810e424177c1c10bd399e38
Phaedra <phaedra@phaedra.net>
2017-10-27 15:31:42 -0400

    added remember me, padding

--- GUIs/login_gui.py
+++ GUIs/login_gui.py
@@ -10,6 +10,7 @@ class login_gui(Tkinter.Tk):
         self.initialize()
 
     def initialize(self):
+        pad = 15
         self.grid()
         
         self.userstr= Tkinter.StringVar()
@@ -19,10 +20,10 @@ class login_gui(Tkinter.Tk):
 
         userlabel = Tkinter.Label(self, textvariable=self.userstr, anchor="n", 
                                   fg="white", bg="orange")
-        userlabel.grid(column=0, row=0, columnspan=2, sticky="EW")
+        userlabel.grid(column=0, row=0, columnspan=2, sticky="EW", pady=pad)
         passwordlabel = Tkinter.Label(self, textvariable=self.passwordstr, anchor="n", 
                                   fg="white", bg="orange")
-        passwordlabel.grid(column=0, row=2, columnspan=2, sticky="EW")
+        passwordlabel.grid(column=0, row=2, columnspan=2, sticky="EW", pady=pad)
 
         #username entry field
         self.entry = Tkinter.Entry(self)
@@ -31,10 +32,15 @@ class login_gui(Tkinter.Tk):
         self.entry = Tkinter.Entry(self)
         self.entry.grid(column=0, row=3, sticky='N')
 
-        button = Tkinter.Button(self, text=u"Login", command="self.onPress")
-        button.grid(column=0, row=4)
+        button = Tkinter.Button(self, text=u"Login", command="self.onPress", bg="orange"
+                                , fg="white")
+        button.grid(column=0, row=5)
         self.grid_columnconfigure(0,weight=1)
         self.resizable(True,False)
+
+        rememberMe = Tkinter.Checkbutton(self, text="Remember Me")
+        rememberMe.grid(column=0, row=4)
+
 
     def onPress(self):
         print "The button was pressed."
