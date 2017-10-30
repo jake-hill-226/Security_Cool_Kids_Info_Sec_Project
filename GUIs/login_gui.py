eeec2c5ac8f8a7f08c0d9a02d79440330abea63b
Phaedra <phaedra@phaedra.net>
2017-10-30 12:36:49 -0400

    Added a GUI font lib

--- GUIs/login_gui.py
+++ GUIs/login_gui.py
@@ -1,6 +1,7 @@
 #!usr/bin/python
 
 import Tkinter
+import tkFont
 
 class login_gui(Tkinter.Tk):
 
@@ -13,33 +14,40 @@ class login_gui(Tkinter.Tk):
         pad = 15
         self.grid()
         
+        self.welcomestr = Tkinter.StringVar()
+        self.welcomestr.set("Welcome To CarrotKey!")
         self.userstr= Tkinter.StringVar()
         self.userstr.set("Username")
         self.passwordstr = Tkinter.StringVar()
         self.passwordstr.set("Password")
+        self.headerfont = tkFont.Font(family="Arial", size=32)
 
+        welcomelabel = Tkinter.Label(self, textvariable=self.welcomestr, anchor="n"
+                                     , fg ="black", font=self.headerfont)
+        welcomelabel.grid(column=0, row=0, columnspan=2, sticky="EW", pady=pad*3, 
+                          padx=pad)
         userlabel = Tkinter.Label(self, textvariable=self.userstr, anchor="n", 
                                   fg="white", bg="orange")
-        userlabel.grid(column=0, row=0, columnspan=2, sticky="EW", pady=pad)
+        userlabel.grid(column=0, row=1, columnspan=2, sticky="EW", pady=pad)
         passwordlabel = Tkinter.Label(self, textvariable=self.passwordstr, anchor="n", 
                                   fg="white", bg="orange")
-        passwordlabel.grid(column=0, row=2, columnspan=2, sticky="EW", pady=pad)
+        passwordlabel.grid(column=0, row=3, columnspan=2, sticky="EW", pady=pad)
 
         #username entry field
         self.entry = Tkinter.Entry(self)
-        self.entry.grid(column=0, row=1, sticky="N")
+        self.entry.grid(column=0, row=2, sticky="N")
         #password entry field
         self.entry = Tkinter.Entry(self)
-        self.entry.grid(column=0, row=3, sticky='N')
+        self.entry.grid(column=0, row=4, sticky='N')
 
         button = Tkinter.Button(self, text=u"Login", command="self.onPress", bg="orange"
                                 , fg="white")
-        button.grid(column=0, row=5)
+        button.grid(column=0, row=6)
         self.grid_columnconfigure(0,weight=1)
         self.resizable(True,False)
 
         rememberMe = Tkinter.Checkbutton(self, text="Remember Me")
-        rememberMe.grid(column=0, row=4)
+        rememberMe.grid(column=0, row=5)
 
 
     def onPress(self):
