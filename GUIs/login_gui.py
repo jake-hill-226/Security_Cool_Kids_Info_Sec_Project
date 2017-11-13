f6ed59245fdbf4c270ccd07d9f522af018a98d81
Phaedra <phaedra@phaedra.net>
2017-11-13 16:24:09 -0500

    minor tweaks to login gui

--- GUIs/login_gui.py
+++ GUIs/login_gui.py
@@ -2,6 +2,7 @@
 
 import Tkinter
 import tkFont
+from PIL import Image, ImageTk
 
 class login_gui(Tkinter.Tk):
 
@@ -13,45 +14,52 @@ class login_gui(Tkinter.Tk):
     def initialize(self):
         pad = 15
         self.grid()
+        ico = ImageTk.PhotoImage(Image.open("../assets/corpCarrot.gif").resize((250,250)))
+        self.icoLab = Tkinter.Label(image=ico)
+        self.icoLab.image = ico
         
+        self.icoLab.grid(column=0, row = 0, sticky="N")
+
         self.welcomestr = Tkinter.StringVar()
         self.welcomestr.set("Welcome To CarrotKey!")
         self.userstr= Tkinter.StringVar()
         self.userstr.set("Username")
         self.passwordstr = Tkinter.StringVar()
         self.passwordstr.set("Password")
-        self.headerfont = tkFont.Font(family="Arial", size=32)
+
+        # change me to lato at some point
+        self.headerfont = tkFont.Font(family="futura", size=32)
 
         welcomelabel = Tkinter.Label(self, textvariable=self.welcomestr, anchor="n"
                                      , fg ="black", font=self.headerfont)
-        welcomelabel.grid(column=0, row=0, columnspan=2, sticky="EW", pady=pad*3, 
+        welcomelabel.grid(column=0, row=1, columnspan=2, sticky="EW", pady=10, 
                           padx=pad)
         userlabel = Tkinter.Label(self, textvariable=self.userstr, anchor="n", 
                                   fg="white", bg="orange")
-        userlabel.grid(column=0, row=1, columnspan=2, sticky="EW", pady=pad)
+        userlabel.grid(column=0, row=2, columnspan=2, sticky="EW", pady=pad)
         passwordlabel = Tkinter.Label(self, textvariable=self.passwordstr, anchor="n", 
                                   fg="white", bg="orange")
-        passwordlabel.grid(column=0, row=3, columnspan=2, sticky="EW", pady=pad)
+        passwordlabel.grid(column=0, row=4, columnspan=2, sticky="EW", pady=pad)
 
         #username entry field
         self.entry = Tkinter.Entry(self)
-        self.entry.grid(column=0, row=2, sticky="N")
+        self.entry.grid(column=0, row=3, sticky="N")
         #password entry field
         self.entry = Tkinter.Entry(self)
-        self.entry.grid(column=0, row=4, sticky='N')
+        self.entry.grid(column=0, row=5, sticky='N')
 
         button = Tkinter.Button(self, text=u"Login", command="self.onPress", bg="orange"
                                 , fg="white")
-        button.grid(column=0, row=6)
+        button.grid(column=0, row=7, pady=(0, pad))
         self.grid_columnconfigure(0,weight=1)
         self.resizable(True,False)
 
         rememberMe = Tkinter.Checkbutton(self, text="Remember Me")
-        rememberMe.grid(column=0, row=5)
+        rememberMe.grid(column=0, row=6)
 
 
     def onPress(self):
-        print "The button was pressed."
+        print("The button was pressed.")
 
 if __name__ == "__main__":
     # the class has no parent becaue it is the root
