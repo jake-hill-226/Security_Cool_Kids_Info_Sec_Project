12332055babb164348a2af3db29607d0fba192e9
Phaedra Paul <phaedra@phaedra.net>
2017-11-14 19:40:17 -0500

    WIP on gui stuff of course. curretly have grid and login

--- GUIs/grid_gui.py
+++ GUIs/grid_gui.py
@@ -2,6 +2,8 @@
 
 import tkFont
 import Tkinter as tk
+import ctypes
+from PIL import Image, ImageTk
 
 class Account(tk.Tk):
     def __init__(self, *args, **kwargs):
@@ -20,35 +22,42 @@ class Account(tk.Tk):
                                      , fg ="black", font=self.headerfont)
         welcomelabel.grid(pady=10, 
                           padx=15, column=0, row=0) 
-        # code should look like for each $acc draw row 
-        header = ["", "Account", "Username", "Password"]
-        self.rect = {}
-        for column in range(4):
-            for row in range(1):
-                x1 = column * self.cellwidth
-                y1 = row * self.cellheight
-                x2 = x1 + self.cellwidth
-                y2 = y1 + self.cellheight
-                xC = (x1+x2)/2
-                yC = (y1+y2)/2
-                # dont draw box at 0,0
-                if column == 0 and row == 0:
-                    pass
-                #numbered boxes far left are slightly smaller than their larger counterparts
-                elif column == 0 and row != 0:
-                   self.rect[row,column] = self.canvas.create_rectangle(x1,y1,x2,y2, fill="lightgrey", tags="rect")
-                   self.canvas.create_text((xC,yC), text=row)
-                elif row == 0 and column != 0:
-                    self.rect[row,column] = self.canvas.create_rectangle(x1,y1,x2,y2, fill="orange", tags="rect")
-                    self.canvas.create_text((xC, yC), text=header[column])
-                else: 
-                    self.rect[row,column] = self.canvas.create_rectangle(x1,y1,x2,y2, fill="white", tags="rect")
+        buildTable(self)
+        self.logoutimg = ImageTk.PhotoImage(Image.open("../assets/logout.png").resize((100,50)))
+        self.settingsimg =  ImageTk.PhotoImage(Image.open("../assets/settings.png").resize((50,50)))
+        self.settings = tk.Label(image= self.settingsimg)
+        self.settings.image = self.settingsimg
 
+        self.settings.grid(column=0, row=1, sticky="ne")
+ 
+def buildTable(self):
+    header = ["", "Account", "Username", "Password"]
+    self.rect = {}
+    for column in range(4):
+        for row in range(1):
+            x1 = column * self.cellwidth
+            y1 = row * self.cellheight
+            x2 = x1 + self.cellwidth
+            y2 = y1 + self.cellheight
+            xC = (x1+x2)/2
+            yC = (y1+y2)/2
+            # dont draw box at 0,0
+            if column == 0 and row == 0:
+                pass
+            #numbered boxes far left are slightly smaller than their larger counterparts
+            elif column == 0 and row != 0:
+               self.rect[row,column] = self.canvas.create_rectangle(x1,y1,x2,y2, fill="lightgrey", tags="rect")
+               self.canvas.create_text((xC,yC), text=row)
+            elif row == 0 and column != 0:
+                self.rect[row,column] = self.canvas.create_rectangle(x1,y1,x2,y2, fill="orange", tags="rect")
+                self.canvas.create_text((xC, yC), text=header[column])
+            else: 
+               self.rect[row,column] = self.canvas.create_rectangle(x1,y1,x2,y2, fill="white", tags="rect")
 
-#@params account to add
-def addAccount():
-    pass
-    
+def popup(title, text, style):
+    return ctypes.windll.user32.MessageBoxW(0, text, title, style)
+
 if __name__ == "__main__":
     acc = Account()
+    acc.title('CarrotKey')
     acc.mainloop()
--- GUIs/login_gui.py
+++ GUIs/login_gui.py
@@ -1,69 +1,85 @@
 #!usr/bin/python
 
-import Tkinter
+import Tkinter as tk
 import tkFont
 from PIL import Image, ImageTk
+import ctypes
+import grid_gui
 
-class login_gui(Tkinter.Tk):
+class login_gui(tk.Tk):
 
     def __init__(self, parent):
-        Tkinter.Tk.__init__(self, parent)
-        self.parent = parent
+        tk.Tk.__init__(self, parent)
+        self.grid()
         self.initialize()
-
+    
     def initialize(self):
         pad = 15
-        self.grid()
         ico = ImageTk.PhotoImage(Image.open("../assets/corpCarrot.gif").resize((250,250)))
-        self.icoLab = Tkinter.Label(image=ico)
+        self.icoLab = tk.Label(image=ico)
         self.icoLab.image = ico
         
         self.icoLab.grid(column=0, row = 0, sticky="N")
 
-        self.welcomestr = Tkinter.StringVar()
+        self.welcomestr = tk.StringVar()
         self.welcomestr.set("Welcome To CarrotKey!")
-        self.userstr= Tkinter.StringVar()
+        self.userstr= tk.StringVar()
         self.userstr.set("Username")
-        self.passwordstr = Tkinter.StringVar()
+        self.passwordstr = tk.StringVar()
         self.passwordstr.set("Password")
 
         # change me to lato at some point
         self.headerfont = tkFont.Font(family="futura", size=32)
 
-        welcomelabel = Tkinter.Label(self, textvariable=self.welcomestr, anchor="n"
+        welcomelabel = tk.Label(self, textvariable=self.welcomestr, anchor="n"
                                      , fg ="black", font=self.headerfont)
         welcomelabel.grid(column=0, row=1, columnspan=2, sticky="EW", pady=10, 
                           padx=pad)
-        userlabel = Tkinter.Label(self, textvariable=self.userstr, anchor="n", 
+        userlabel = tk.Label(self, textvariable=self.userstr, anchor="n", 
                                   fg="white", bg="orange")
         userlabel.grid(column=0, row=2, columnspan=2, sticky="EW", pady=pad)
-        passwordlabel = Tkinter.Label(self, textvariable=self.passwordstr, anchor="n", 
+        passwordlabel = tk.Label(self, textvariable=self.passwordstr, anchor="n", 
                                   fg="white", bg="orange")
         passwordlabel.grid(column=0, row=4, columnspan=2, sticky="EW", pady=pad)
 
         #username entry field
-        self.entry = Tkinter.Entry(self)
+        self.entry = tk.Entry(self)
         self.entry.grid(column=0, row=3, sticky="N")
         #password entry field
-        self.entry = Tkinter.Entry(self)
+        self.entry = tk.Entry(self, show=u"*")
         self.entry.grid(column=0, row=5, sticky='N')
 
-        button = Tkinter.Button(self, text=u"Login", command="self.onPress", bg="orange"
-                                , fg="white")
-        button.grid(column=0, row=7, pady=(0, pad))
-        self.grid_columnconfigure(0,weight=1)
-        self.resizable(True,False)
+        self.button = tk.Button(self)
+        self.button["text"] ="Login"
+        self.button["command"] = self.checkAuth
 
-        rememberMe = Tkinter.Checkbutton(self, text="Remember Me")
+        self.button.grid(column=0, row=7, pady=(0, pad))
+        self.grid_columnconfigure(0,weight=1)
+        self.resizable(False,False)
+
+        rememberMe = tk.Checkbutton(self, text="Remember Me")
         rememberMe.grid(column=0, row=6)
 
+    def checkAuth(self):
+        auth = False
+        if not auth:
+            self.onDeny()
 
-    def onPress(self):
-        print("The button was pressed.")
+    def onDeny(self):
+        self.exitico = ImageTk.PhotoImage(Image.open("../assets/logout.png").resize((100,50)))
+        self.denyEntry = tk.Label(self, text="The Password or Username Entered Was Incorrect.")
+
+    def onAuth(self):
+        self.destroy()
+        grid_gui.Account()
 
 if __name__ == "__main__":
     # the class has no parent becaue it is the root
     app = login_gui(None)
+    app.geometry("400x600")
     app.title('CarrotKey')
+
+    def destroy(self):
+        app.destroy()
     # program will loop to wait for events. 
     app.mainloop()
